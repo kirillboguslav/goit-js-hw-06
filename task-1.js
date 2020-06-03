09905ac183c4c55392e0788860fe84f2055791d8
kirillboguslav <kirill.boguslav@gmail.com>
2020-06-03 23:06:59 +0300

    task1

--- task-1.js
+++ task-1.js
@@ -1,1 +1,52 @@
 'use strict';
+import users from './users.js';
+
+//Получить массив имен всех пользователей (поле name).
+
+const getUserNames = users => {
+  // твой код
+  return users.map(user => user.name);
+};
+
+console.log(getUserNames(users));
+// [ 'Moore Hensley', 'Sharlene Bush', 'Ross Vazquez', 'Elma Head', 'Carey Barr', 'Blackburn Dotson', 'Sheree Anthony' ]
+
+///////////////////////////////////////////////////////////////
+//Получить массив объектов пользователей по цвету глаз (поле eyeColor).
+
+const getUsersWithEyeColor = (users, color) => {
+  // твой код
+  return users.filter(user => user.eyeColor === color);
+};
+
+console.log(getUsersWithEyeColor(users, 'blue')); // [объект Moore Hensley, объект Sharlene Bush, объект Carey Barr]
+
+///////////////////////////////////////////////////////////////
+//Получить массив имен пользователей по полу (поле gender).
+
+const getUsersWithGender = (users, gender) => {
+  // твой код
+  return users.filter(user => user.gender === gender).map(user => user.name);
+};
+
+console.log(getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
+
+///////////////////////////////////////////////////////////////
+//Получить массив только неактивных пользователей (поле isActive).
+
+const getInactiveUsers = users => {
+  // твой код
+  return users.filter(user => !user.isActive);
+};
+
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
+
+///////////////////////////////////////////////////////////////
+//Получить пользоваля (не массив) по email (поле email, он уникальный).
+
+const getUserWithEmail = (users, email) => {
+  // твой код
+};
+
+console.log(getUserWithEmail(users, 'shereeanthony@kog.com')); // {объект пользователя Sheree Anthony}
+console.log(getUserWithEmail(users, 'elmahead@omatom.com')); // {объект пользователя Elma Head}
